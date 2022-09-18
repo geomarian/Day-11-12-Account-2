31a42c1ca68b3e1ead74333f1dd4f9f6f3eb8e4d
geomarian <geomarian520@gmail.com>
2022-09-18 23:20:42 +0530

    Added Account file

---
+++ src/com/bridgelabz/Account.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author ASUS
+ *
+ */
+public class Account {
+	
+	public static int ACCOUNT_BALANCE;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccountTest testAcc = new AccountTest();
+		testAcc.debitMoney();
+		testAcc.debitWholeCash();
+		
+		
+}
+	
+}
